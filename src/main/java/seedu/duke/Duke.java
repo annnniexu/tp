264d166b89549cfd4d9af168736e964d1b816f2b
EugeneChanJiajun <e0969415@u.nus.edu>
2024-04-01 11:47:48 +0800

    Add CheckParameters class to handle input exceptions

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -3,7 +3,11 @@ import java.io.IOException;
 import java.time.DateTimeException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
-import java.util.logging.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.LogManager;
 
 public class Duke {
 
